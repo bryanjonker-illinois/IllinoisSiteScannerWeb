4f11c54ec477018acc8021a9248ad5ff2aa8347f
Bryan Jonker <jonker@illinois.edu>
2026-05-15 12:18:50 -0500

    Add api

---
+++ IllinoisSiteScannerWeb/Controllers/ScannerController.cs
@@ -0,0 +1,13 @@
+﻿using IllinoisSiteScannerWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IllinoisSiteScannerWeb.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScannerController : ControllerBase {
+        public async Task<IActionResult> Index(string url) {
+            var result = await WebsiteManager.Load(url);
+            return new JsonResult(result);
+        }
+    }
+}
